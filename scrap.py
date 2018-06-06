af7c0bb48551a776483a05c2bba4717116875d5d
Sunil-MI <sunillulla546@yahoo.com>
2018-06-07 00:39:56 +0530

    feat(Scrap): Python scraping added for spscific pages

--- scrap.py
+++ scrap.py
@@ -17,11 +17,12 @@ for i,sneakers in enumerate(sneakers_arr):
 		"altText":"",
 		"title":"",
 		"price":0,
-		"starRating":"",
+		"starRating":0,
 		"starRatingBy":0,
 		"totalReviews":"",
 		"totalReviewsBy":0,
-		"sizes":"",
+		"sizes":[],
+		"images":[],
 		"availableColors":[]
 
 	};
@@ -32,16 +33,17 @@ for i,sneakers in enumerate(sneakers_arr):
 	sneakers_json[i]["title"] = img_data["alt"]
 	sneakers_json[i]["coverImage"] = img_data["src"]
 	star_rating_div = sneakers.find("div",{"class":"niH0FQ _36Fcw_"})
-	if star_rating_div!= None: #if Rating is available
+	if star_rating_div: #if Rating is available
 		star_rating_spans = star_rating_div.find_all("span")
 		sneakers_json[i]["starRating"] = star_rating_spans[0].find("div").getText() # write a logic to parse this
 		sneakers_json[i]["starRatingBy"] = star_rating_spans[1].getText() # write a logic to parse this
 
 	sneakers_json[i]["price"] = sneakers.find("div",{"class":"_1vC4OE"}).getText();
 
+	sneaker_page_url = 'https://www.flipkart.com'+sneakers_json[i]["pageURL"]
+	shoe_page_data = requests.get(sneaker_page_url)
+	print shoe_page_data.content
 
-		
-	
 	#print starRating
 	
 
